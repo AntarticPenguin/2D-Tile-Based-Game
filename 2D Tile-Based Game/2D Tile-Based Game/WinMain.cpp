43060b367819522988733ac517a9c17439c3d1f6
박평기 <kes99709@gmail.com>
2017-09-19 19:53:12 +0900

    170919 기본 윈도우 생성
    기본 윈도우 생성

---
+++ 2D Tile-Based Game/2D Tile-Based Game/WinMain.cpp
@@ -0,0 +1,48 @@
+#include <Windows.h>
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int nCmdShow)
+{
+	WNDCLASS wc;
+
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpfnWndProc = WndProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = LoadIcon(0, IDI_APPLICATION);
+	wc.hCursor = LoadCursor(0, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wc.lpszMenuName = 0;
+	wc.lpszClassName = L"Base";
+
+	if (!RegisterClass(&wc))
+	{
+		return 0;
+	}
+
+	HWND hWnd = CreateWindow(
+		L"Base",
+		L"¸ÚÁøÀÌ¸§",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,			//x
+		CW_USEDEFAULT,			//y
+		CW_USEDEFAULT,			//Width
+		CW_USEDEFAULT,			//Height
+		0,
+		0,
+		hInstance,
+		0
+	);
+
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	return 0;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
